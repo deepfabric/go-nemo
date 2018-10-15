1ccf8abfe14dec24082656bb177c759f5551d36b
Zhichang Yu <yuzhichang@gmail.com>
2018-10-15 14:51:00 +0800

    adjusted jemalloc link order

--- c-nemo.go
+++ c-nemo.go
@@ -3,7 +3,7 @@ package gonemo
 // #cgo CXXFLAGS: -std=c++11
 // #cgo CPPFLAGS: -I../c-nemo/internal/include
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
-// #cgo LDFLAGS: -lstdc++ -lsnappy -lbz2 -lz -ljemalloc -lm
+// #cgo LDFLAGS: -lsnappy -lbz2 -lz -ljemalloc -lm -lstdc++
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all -lrt -lpthread
 import "C"
